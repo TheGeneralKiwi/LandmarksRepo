5edf7a2aee7e936e3e17967178bbd075d7a29cec
Jasmine Teutsch <jaspeach@outlook.com>
2023-08-22 17:06:28 -0400

    WE DID IT, WE SUCCESSFULLY COPIED EVERY LAST ONE, NO ERRORS, XCODE DIDN'T HAVE A SEIZURE, WOO BABY WOO

---
+++ LandmarksRepo/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks_
+//
+//  Created by Jasmine Tetusch on 6/15/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
---
+++ LandmarksRepo/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks_
+//
+//  Created by Jasmine Tetusch on 5/24/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
