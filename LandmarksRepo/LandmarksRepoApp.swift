2eddb32247436bc4e4b41492f63a1999cb80a77b
Jasmine Teutsch <jaspeach@outlook.com>
2023-08-22 16:34:31 -0400

    Initial Commit

---
+++ LandmarksRepo/LandmarksRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksRepoApp.swift
+//  LandmarksRepo
+//
+//  Created by Jasmine Tetusch on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
